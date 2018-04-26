b2a6b9e9f0e20916697eebaea0ba586d40ab16ef
Kristen Hare <sailorgoon@gmail.com>
2018-04-26 14:31:09 -0500

    made food schema, mongoose installed

--- server/routes/food.route.js
+++ server/routes/food.route.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const databaseUrl = 'mongodb://localhost:27017/kitchen'
 
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', () => {
+    console.log(`mongoose connected to ${databaseUrl}`)
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('mongoose connection error')
+})
 const food = [];
 
 router.get('/', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static('server/public'));
 
 
 
-const food = [];
+
 
 app.use('/food', foodRouter);
 
